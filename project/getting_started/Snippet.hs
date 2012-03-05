cfc6961b38ee0423f311abaffbc45767e3cd5da7
Russell <miller.time.baby@gmail.com>
2012-03-05 11:56:51 -0800

    playing with bachs prelude some more

--- project/getting_started/Snippet.hs
+++ project/getting_started/Snippet.hs
@@ -4,4 +4,4 @@ import Haskore.Music.GeneralMIDI as MidiMusic
 import Haskore.Interface.MIDI.Render as Render
 
 render_to f m = Render.fileFromGeneralMIDIMusic f song where
-  song = MidiMusic.fromMelodyNullAttr MidiMusic.AcousticGrandPiano m
+  song = MidiMusic.fromMelodyNullAttr MidiMusic.Marimba m
