26cf1d3ca389b297920773e5b1eb5e36e5116cd2
Russell <miller.time.baby@gmail.com>
2012-03-08 23:21:40 -0800

    starting the project code

---
+++ project/HaskBeatz.hs
@@ -0,0 +1,16 @@
+module HaskBeatz where
+
+import Haskore.Basic.Dynamics
+import Haskore.Melody
+import Haskore.Music.GeneralMIDI as MidiMusic
+import Haskore.Interface.MIDI.Render as Render
+
+main = do putStr "Enter a file name: "
+          fname <- getLine
+          render_to fname melody
+          putStrLn ""
+
+render_to f m = Render.fileFromGeneralMIDIMusic f song where
+  song = MidiMusic.fromMelodyNullAttr MidiMusic.AcousticGrandPiano m
+
+melody = c 1 4 ()
